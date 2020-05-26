c351edbca5f7892b0fef12e4416f8f505dbcaf07
uedakeita2 <painame0129@gmail.com>
2020-05-26 15:27:10 +0900

    追加

--- Remote Project/Scene/CharSelScene.cpp
+++ Remote Project/Scene/CharSelScene.cpp
@@ -1,4 +1,7 @@
 #include "CharSelScene.h"
+#include "DxLib.h"
+#include "GameScene.h"
+#include "SceneMng.h"
 
 CharSelScene::CharSelScene()
 {
@@ -11,5 +14,13 @@ CharSelScene::~CharSelScene()
 
 unique_Base CharSelScene::Update(unique_Base own)
 {
+	PadInput = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+	EPadInput = GetJoypadInputState(DX_INPUT_PAD2);
 
+	if (CheckHitKey(KEY_INPUT_Z))
+	{
+		return std::make_unique<GameScene>();
+	}
+
+	return std::move(own);
 }
--- Remote Project/Scene/CharSelScene.h
+++ Remote Project/Scene/CharSelScene.h
@@ -3,8 +3,11 @@
 class CharSelScene :
 	public BaseScene
 {
+public:
 	CharSelScene();
 	~CharSelScene();
 	unique_Base Update(unique_Base own);
+	int PadInput;
+	int EPadInput;
 };
 
